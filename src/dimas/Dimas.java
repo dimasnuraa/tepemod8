44e70b113a1696a83f56a78ed1989854192d1faa
Praktikum <Praktikum@Lenovo-PC>
2015-04-07 08:07:51 +0700

    new commit

---
+++ src/dimas/Dimas.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dimas;
+
+/**
+ *
+ * @author Praktikum
+ */
+public class Dimas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Dimas Nur Abdillah - 6306130034");
+        System.out.println("Saya sangat suka praktikum rpl modul 8 ini");
+    }
+    
+}
